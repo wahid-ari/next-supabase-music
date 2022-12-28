ee57ae315b64a408559adf7a95d290b47ab3d8ac
wahidari <wahiid.ari@gmail.com>
2022-12-28 16:41:35 +0700

    update

--- pages/song/add.js
+++ pages/song/add.js
@@ -100,9 +100,9 @@ export default function AddSong() {
           <Combobox value={selectedArtist} onChange={setSelectedArtist}>
             <div className="relative mt-1 pb-1">
               <Label>Artist</Label>
-              <div className="relative my-2 w-full cursor-default overflow-hidden rounded text-left border border-neutral-300 dark:border-neutral-600 text-sm">
+              <div className="relative my-2 w-full cursor-default overflow-hidden rounded-md text-left border border-neutral-300 dark:border-neutral-600 text-sm">
                 <Combobox.Input
-                  className="w-full py-2.5 pl-3 pr-10 text-sm text-neutral-900 dark:text-white dark:bg-neutral-900 rounded border border-transparent focus:border-emerald-500"
+                  className="w-full py-2 pl-3 pr-10 text-sm text-neutral-900 dark:text-white dark:bg-neutral-900 rounded-md border border-transparent focus:border-emerald-500"
                   displayValue={(artist) => artist?.name}
                   placeholder="Search Artist"
                   onChange={(e) => setQueryArtist(e.target.value)}
@@ -169,9 +169,9 @@ export default function AddSong() {
           <Combobox value={selectedAlbum} onChange={setSelectedAlbum}>
             <div className="relative mt-1 pb-3">
               <Label>Album (Optional)</Label>
-              <div className="relative my-2 w-full cursor-default overflow-hidden rounded text-left border border-neutral-300 dark:border-neutral-600 text-sm">
+              <div className="relative my-2 w-full cursor-default overflow-hidden rounded-md text-left border border-neutral-300 dark:border-neutral-600 text-sm">
                 <Combobox.Input
-                  className="w-full py-2.5 pl-3 pr-10 text-sm text-neutral-900 dark:text-white dark:bg-neutral-900  rounded border border-transparent focus:border-emerald-500"
+                  className="w-full py-2 pl-3 pr-10 text-sm text-neutral-900 dark:text-white dark:bg-neutral-900 rounded-md border border-transparent focus:border-emerald-500"
                   displayValue={(album) => album?.name}
                   placeholder="Search Album"
                   onChange={(e) => setQueryAlbum(e.target.value)}
--- pages/song/edit/[id].js
+++ pages/song/edit/[id].js
@@ -129,9 +129,9 @@ export default function Song({ id }) {
             <Combobox value={selectedArtist} onChange={setSelectedArtist}>
               <div className="relative mt-1 pb-1">
                 <Label>Artist</Label>
-                <div className="relative my-2 w-full cursor-default overflow-hidden rounded text-left border border-neutral-300 dark:border-neutral-600 text-sm">
+                <div className="relative my-2 w-full cursor-default overflow-hidden rounded-md text-left border border-neutral-300 dark:border-neutral-600 text-sm">
                   <Combobox.Input
-                    className="w-full py-2.5 pl-3 pr-10 text-sm text-neutral-900 dark:text-white dark:bg-neutral-900 rounded border border-transparent focus:border-emerald-500"
+                    className="w-full py-2 pl-3 pr-10 text-sm text-neutral-900 dark:text-white dark:bg-neutral-900 rounded-md border border-transparent focus:border-emerald-500"
                     displayValue={(artist) => artist?.name}
                     placeholder="Search Artist"
                     onChange={(e) => setQueryArtist(e.target.value)}
@@ -198,9 +198,9 @@ export default function Song({ id }) {
             <Combobox value={selectedAlbum} onChange={setSelectedAlbum}>
               <div className="relative mt-1 pb-3">
                 <Label>Album (Optional)</Label>
-                <div className="relative my-2 w-full cursor-default overflow-hidden rounded text-left border border-neutral-300 dark:border-neutral-600 text-sm">
+                <div className="relative my-2 w-full cursor-default overflow-hidden rounded-md text-left border border-neutral-300 dark:border-neutral-600 text-sm">
                   <Combobox.Input
-                    className="w-full py-2.5 pl-3 pr-10 text-sm text-neutral-900 dark:text-white dark:bg-neutral-900 rounded border border-transparent focus:border-emerald-500"
+                    className="w-full py-2 pl-3 pr-10 text-sm text-neutral-900 dark:text-white dark:bg-neutral-900 rounded-md border border-transparent focus:border-emerald-500"
                     displayValue={(album) => album?.name}
                     placeholder="Search Album"
                     onChange={(e) => setQueryAlbum(e.target.value)}
