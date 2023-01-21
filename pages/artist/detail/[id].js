9f9cda2354596de41d2306bc99b24bc975882b91
wahidari <wahiid.ari@gmail.com>
2023-01-21 13:29:05 +0700

    change artist focus state to focus-visible

--- pages/artist/detail/[id].js
+++ pages/artist/detail/[id].js
@@ -113,7 +113,7 @@ export default function Album({ id }) {
         Cell: (row) => {
           const { values, original } = row.cell.row;
           return (
-            <Link href={`/song/detail/${original.id}`} className="text-emerald-500 hover:text-emerald-600 text-sm font-medium focus:outline-none focus:ring-1 focus:ring-emerald-500 rounded">
+            <Link href={`/song/detail/${original.id}`} className="text-emerald-500 hover:text-emerald-600 text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-emerald-500 rounded">
               {values.name}
             </Link>
           )
@@ -163,7 +163,7 @@ export default function Album({ id }) {
         Cell: (row) => {
           const { values, original } = row.cell.row;
           return (
-            <Link href={`/album/detail/${original.id}`} className="text-emerald-500 hover:text-emerald-600 text-sm font-medium focus:outline-none focus:ring-1 focus:ring-emerald-500 rounded">
+            <Link href={`/album/detail/${original.id}`} className="text-emerald-500 hover:text-emerald-600 text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-emerald-500 rounded">
               {values.name}
             </Link>
           )
